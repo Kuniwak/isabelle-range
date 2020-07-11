6d196171bda509fb6ad63de382d4ed96e1cefa24
Kuniwak <orga.chem.job@gmail.com>
2020-07-11 17:37:59 +0900

    (a, b) that a > b is not a range

--- range.thy
+++ range.thy
@@ -1,7 +1,57 @@
 theory range imports Main begin
 
 definition R :: "(nat \<times> nat) set" where
-  "R == { (x, y) | x y. True }"
+  "R == { (x, y) | x y. x \<le> y }"
+
+lemma eq_le_le1: "(c::nat) = a \<longrightarrow> a \<le> b \<longrightarrow> c \<le> a"
+  apply(intro impI)
+  apply(erule subst)
+  apply(rule eq_refl)
+  apply(rule refl)
+  done
+
+lemma eq_le_le2: "(c::nat) = a \<longrightarrow> b \<le> a \<longrightarrow> b \<le> c"
+  apply(intro impI)
+  apply(erule ssubst)
+  apply(assumption)
+  done
+
+lemma le_le_trans: "(a::nat) \<le> b \<longrightarrow> b \<le> c \<longrightarrow> a \<le> c"
+  apply(intro impI)
+  apply(case_tac "a=b")
+  apply(erule ssubst)
+  apply(assumption)
+  apply(drule_tac a=a and b=b in le_neq_trans)
+  apply(assumption)
+  apply(case_tac "b=c")
+  apply(drule_tac x=a and y=b and z=c in less_le_trans)
+  apply(assumption)
+  apply(erule less_imp_le)
+  apply(drule le_neq_trans)
+  apply(assumption)
+  apply(drule_tac x=a and y=b and z=c in less_trans)
+  apply(assumption)
+  apply(erule less_imp_le)
+  done
+
+theorem "a > b \<Longrightarrow> (a, b) \<notin> R"
+  apply(unfold R_def)
+  apply(rule notI)
+  apply(erule CollectE)
+  apply(elim exE)
+  apply(erule conjE)
+  apply(erule Pair_inject)
+  apply(drule_tac a=x and b=y in eq_le_le1[rule_format])
+  apply(assumption)
+  apply(drule sym)
+  apply(drule_tac a=x and b=y and c=b in ord_le_eq_trans)
+  apply(assumption)
+  apply(drule_tac a=a and b=x and c=b in le_le_trans[rule_format])
+  apply(assumption)
+  apply(drule_tac y=a in leD)
+  apply(erule notE)
+  apply(assumption)
+  done
 
 fun lower_bound :: "(nat \<times> nat) \<Rightarrow> nat" where
   "lower_bound (x, y) = x"
@@ -41,7 +91,7 @@ lemma example_3_8_in_R: "(3, 8) \<in> R"
   apply(rule_tac x=8 in exI)
   apply(rule conjI)
   apply(rule refl)
-  apply(rule TrueI)
+  apply(simp)
   done
 
 theorem example_3_8: "r = (3, 8) \<Longrightarrow>
@@ -62,37 +112,6 @@ theorem example_3_8: "r = (3, 8) \<Longrightarrow>
   apply(rule refl)
   apply(rule less_imp_le_nat)
   apply(auto)
-  done
-
-lemma eq_le_le1: "(c::nat) = a \<longrightarrow> a \<le> b \<longrightarrow> c \<le> a"
-  apply(intro impI)
-  apply(erule subst)
-  apply(rule eq_refl)
-  apply(rule refl)
-  done
-
-lemma eq_le_le2: "(c::nat) = a \<longrightarrow> b \<le> a \<longrightarrow> b \<le> c"
-  apply(intro impI)
-  apply(erule ssubst)
-  apply(assumption)
-  done
-
-lemma le_le_trans: "(a::nat) \<le> b \<longrightarrow> b \<le> c \<longrightarrow> a \<le> c"
-  apply(intro impI)
-  apply(case_tac "a=b")
-  apply(erule ssubst)
-  apply(assumption)
-  apply(drule_tac a=a and b=b in le_neq_trans)
-  apply(assumption)
-  apply(case_tac "b=c")
-  apply(drule_tac x=a and y=b and z=c in less_le_trans)
-  apply(assumption)
-  apply(erule less_imp_le)
-  apply(drule le_neq_trans)
-  apply(assumption)
-  apply(drule_tac x=a and y=b and z=c in less_trans)
-  apply(assumption)
-  apply(erule less_imp_le)
   done
 
 theorem example_3_8_w: "\<lbrakk> r = (3, 8); E = { x | x. 3 \<le> x \<and> x \<le> 8 } \<rbrakk> \<Longrightarrow>
